2326293e441980ee485739e43191c9362efe5d17
MASATAKA-FUKAYA <masataka.f@fukayashoukounoMacBook-Air.local>
2022-01-16 19:38:34 +0900

    fix link-href to common.css

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
         <title>Scout! | <?php echo $title ?></title>
         <link rel="stylesheet" href="css/ress.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-        <link rel="stylesheet" href="css/common.css">
+        <link rel="stylesheet" href="common.css">
         <link rel="stylesheet" href=<?php if(!empty($originalCss)) echo $originalCss; ?>>
         <link rel="stylesheet" href=<?php if(!empty($originalCss2)) echo $originalCss2; ?>>
     </head>
